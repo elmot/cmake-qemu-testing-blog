d8e6acc90d933c2cc541341eed6ea22211e08e15
Ilia (Elias) Motornyi <elijah.mot@gmail.com>
2022-09-02 14:07:49 +0300

    Use CMake FetchContent

--- cmake/cmsis.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT EXISTS ${CMAKE_SOURCE_DIR}/CMSIS/ARM.CMSIS.pdsc)
-    message(STATUS "Download CMSIS ...")
-    file(DOWNLOAD "https://github.com/ARM-software/CMSIS_5/releases/download/5.9.0/ARM.CMSIS.5.9.0.pack" cmsis.zip TIMEOUT 300)
-    message(STATUS "Extract CMSIS ...")
-    file(ARCHIVE_EXTRACT
-            INPUT cmsis.zip
-            DESTINATION "${CMAKE_SOURCE_DIR}/CMSIS")
-    file(REMOVE ${PROJECT_BINARY_DIR}/cmsis.zip)
-endif()
