6a9c56910f85d4fd621afbbfbaeb96640453c4a1
Ilia (Elias) Motornyi <elijah.mot@gmail.com>
2022-09-05 15:19:34 +0300

    Add CMSIS DSP test Reformat code

--- qemu_testing/qemu_testing.cmake
+++ qemu_testing/qemu_testing.cmake
@@ -1,8 +1,8 @@
 include(FetchContent)
 include(CTest)
 
-FetchContent_Declare(cmsis5_for_tests URL https://github.com/ARM-software/CMSIS_5/releases/download/5.9.0/ARM.CMSIS.5.9.0.pack)
-FetchContent_Populate(cmsis5_for_tests)
+FetchContent_Declare(cmsis5 URL https://github.com/ARM-software/CMSIS_5/releases/download/5.9.0/ARM.CMSIS.5.9.0.pack)
+FetchContent_Populate(cmsis5)
 
 find_program(QEMU_BIN_FOR_TEST qemu-system-arm
         REQUIRED
@@ -10,7 +10,7 @@ find_program(QEMU_BIN_FOR_TEST qemu-system-arm
         HINTS "C:/Program\ Files/qemu")
 
 message(STATUS "QEMU executable found: ${QEMU_BIN_FOR_TEST}")
-set_property(SOURCE QemuArmTesting/testing_main.c APPEND_STRING PROPERTY COMPILE_FLAGS "-include \"${CMSIS_DEVICE_OPT}.h\"")
+set_property(SOURCE qemu_testing/qemu_testing.c APPEND_STRING PROPERTY COMPILE_FLAGS "-include \"${CMSIS_DEVICE_OPT}.h\"")
 
 
 function(add_qemu_test name )
@@ -20,17 +20,17 @@ function(add_qemu_test name )
     target_compile_definitions(${name} PRIVATE ${CMSIS_DEVICE_OPT})
 
     target_include_directories(${name} PRIVATE
-            ${cmsis5_for_tests_SOURCE_DIR}/Device/ARM/${CMSIS_DEVICE}/Include
-            ${cmsis5_for_tests_SOURCE_DIR}/CMSIS/Core/Include
-            QemuArmTesting
+            ${cmsis5_SOURCE_DIR}/Device/ARM/${CMSIS_DEVICE}/Include
+            ${cmsis5_SOURCE_DIR}/CMSIS/Core/Include
+            qemu_testing
             )
 
     target_sources(${name} PRIVATE
-            QemuArmTesting/testing_main.c
-            ${cmsis5_for_tests_SOURCE_DIR}/Device/ARM/${CMSIS_DEVICE}/Source/startup_${CMSIS_DEVICE}.c
-            ${cmsis5_for_tests_SOURCE_DIR}//Device/ARM/${CMSIS_DEVICE}/Source/system_${CMSIS_DEVICE}.c)
+            qemu_testing/qemu_testing.c
+            ${cmsis5_SOURCE_DIR}/Device/ARM/${CMSIS_DEVICE}/Source/startup_${CMSIS_DEVICE}.c
+            ${cmsis5_SOURCE_DIR}/Device/ARM/${CMSIS_DEVICE}/Source/system_${CMSIS_DEVICE}.c)
     target_link_options(${name} PRIVATE
-            -T ${cmsis5_for_tests_SOURCE_DIR}/Device/ARM/${CMSIS_DEVICE}/Source/GCC/gcc_arm.ld
+            -T ${cmsis5_SOURCE_DIR}/Device/ARM/${CMSIS_DEVICE}/Source/GCC/gcc_arm.ld
             --specs=rdimon.specs --specs=nano.specs
             -Wl,-gc-sections,--print-memory-usage,-Map=${PROJECT_BINARY_DIR}/${name}.map
     )
